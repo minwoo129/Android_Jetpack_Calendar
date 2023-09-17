eba785bb23f4ecd932d1f49fd4a0315ff6edd7c7
minwoo129 <key0129mw@naver.com>
2023-09-17 13:12:30 +0900

    캘린더 날짜 출력

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calendar
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -58,6 +60,7 @@ fun CalendarApp() {
         CalendarHeader(time)
         CalendarHeaderBtn(time)
         CalendarDayName()
+        CalendarDayList(time)
     }
 }
 
@@ -134,6 +137,51 @@ fun CalendarDayName() {
             }
         }
     }
+}
+
+@Composable
+fun CalendarDayList(date: MutableState<Calendar>) {
+    // 달력 그리는 공식 -> Jetpack Compose로 어떻게 달력을 그리는지
+
+    date.value.set(Calendar.DAY_OF_MONTH, 1)
+
+    // 월말이 몇일인지 계산
+    val monthDayMax = date.value.getActualMaximum(Calendar.DAY_OF_MONTH)
+    // 이번달 1일이 무슨 요일인지
+    val monhtFirstDay = date.value.get(Calendar.DAY_OF_WEEK) -1;
+    // 이번달이 총 몇 주로 되어 있는지
+    val monthWeeksCount = (monthDayMax + monhtFirstDay + 6) / 7
+
+    Log.d("monthDayMax", monthDayMax.toString())
+    Log.d("monhtFirstDay", monhtFirstDay.toString())
+    Log.d("monthWeeksCount", monthWeeksCount.toString())
+
+    Column {
+        repeat(monthWeeksCount) {week ->
+            Row() {
+                repeat(7) {day ->
+                    val resultDay = week * 7 + day - monhtFirstDay + 1
+                    if(resultDay in 1 .. monthDayMax) {
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(10.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = resultDay.toString(),
+                                fontSize = 15.sp
+                            )
+                        }
+                    }
+                    else {
+                        Spacer(modifier = Modifier.weight(1f))
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
